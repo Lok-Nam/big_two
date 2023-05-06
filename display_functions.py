2896c99c13581d27b6fa39581fcc87dd99d5c92a
Lok <ec211014@qmul.ac.uk>
2023-05-06 14:01:07 +0100

    done comment on display functions

--- display_functions.py
+++ display_functions.py
@@ -44,15 +44,22 @@ bar_height = 50
 bar_rect = pygame.Rect(0, 0, screen_width, bar_height)
 
 def ini_screen():
-    screen.fill((0, 0, 0))
+    """
+    initialize screen by filling screen black
+    """
+    screen.fill(black)
 
 def display_hand(player, chosen):
+    """
+    Display the deck of player at bottom of screen, move the card towards top if the card is chosen by the player.
+    Takes player object and a list of chosen card.
+    """
     cards = player.hand
     num = len(cards)
     offset = (screen_width -( card_width*num ))/2
     for i in range (num):
         if cards[i] in chosen:
-            offsetY = 30
+            offsetY = 30 # offset is added to the cards that are chosen so that they move up.
         else:
             offsetY = 0
         image = cards[i].image.convert()
@@ -63,6 +70,10 @@ def display_hand(player, chosen):
 
 
 def display_last_played(played):
+    """
+    Display the card played by last player.
+    The function takes a list of cards.
+    """
     cards = played[0]
     num = len(cards)
     offsetX = (screen_width -( card_width*num ))/2
@@ -75,11 +86,17 @@ def display_last_played(played):
         screen.blit(image, pos)
 
 def display_buttons(isPlayable, isSkipable):
+    """
+    Display the play and skip buttons if the condition is met. Hover effects are added to the buttons.
+    This function takes two boolean as argument, which determine whether the buttons should be shown or not.
+    """
+    # defining text
     text_play = smallfont.render('play' , True , white)
     text_skip = smallfont.render('skip', True, white)
-    mouse = pygame.mouse.get_pos()
+
+    mouse = pygame.mouse.get_pos() # get mouse position for condition of hovering
     if((mouse[0] > play_button_X and mouse[0] < play_button_X + button_width)and(mouse[1] > play_button_Y and mouse[1] < play_button_Y + button_height) and isPlayable):
-        pygame.draw.rect(screen, dark_blue, play_button_rect)
+        pygame.draw.rect(screen, dark_blue, play_button_rect) # dark colour for hovering effect
         screen.blit(text_play, (play_button_X+20, play_button_Y+20))
     elif(isPlayable):
         pygame.draw.rect(screen, blue, play_button_rect)
@@ -95,9 +112,13 @@ def display_buttons(isPlayable, isSkipable):
     
 
 def display_top_bar(players, index):
+    """
+    Display the top bar, which indicate the number of cards left for each player and the current player.
+    This function takes a list of player and the index of current player.
+    """
     message = ('Current player is Player' + str(index+1) + ', Cards left for each player : ')
     pygame.draw.rect(screen, green, bar_rect)
-    for i in range(len(players)):
+    for i in range(len(players)): # create the message string
         if(i != index):
             message += (' | Player' + str(i+1) + ': ' + str(len(players[i].hand)))
     bar_message = smallfont.render(message, True, black)
